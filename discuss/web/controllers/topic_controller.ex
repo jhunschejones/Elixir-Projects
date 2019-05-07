3bb67142893d02424e6af73262a129dae39f2a4e
Joshua Hunsche Jones <joshua@hunschejones.com>
2019-05-06 22:02:18 -0700

    Add new topic form and controller method

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -1,7 +1,22 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
+  alias Discuss.Topic # allows `Topic` in place of `Discuss.Topic`
   
-  def new do
-    
+  def new(conn, _params) do
+    # create an empty Ecto.Changeset to use with form
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, params) do
+    %{"topic" => topic} = params
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect post
+      {:error, changeset} -> 
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
---
+++ discuss/web/models/topic.ex
@@ -0,0 +1,17 @@
+defmodule Discuss.Topic do
+  use Discuss.Web, :model
+
+  schema "topics" do
+    field :title, :string
+  end
+
+  # struct argument is a record to be entered into the database
+  # params argument represents properties we want to update the struct with
+  # entire function returns an Ecto.Changeset to update the database, including
+  # a `valid?: boolean` response and any errors from failed validation steps
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title]) # produces an Ecto.Changeset
+    |> validate_required([:title]) # checks if Ecto.Changeset is valid
+  end
+end
